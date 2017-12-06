70ad4c5ebad9dc7f620b377e822711c7e05056e2
ramyaksingh <ramyaksingh@yahoo.com>
2017-12-06 03:25:05 -0500

    Sign Up Backend without Database Support

--- main.py
+++ main.py
@@ -13,12 +13,17 @@ Bootstrap(app)
 def index():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
 
-    form = myForms.LoginForm()    
+    form = myForms.LoginForm()
     return render_template('login.html', form=form)
 
+@app.route('/registration', methods=['GET', 'POST'])
+def registration():
+    form = RegisterForm()
+
+    return render_template('signup.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- myForms.py
+++ myForms.py
@@ -3,7 +3,12 @@ from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length
 
 
-class LoginForm():
+class LoginForm(FlaskForm):
     username = StringField('username', validators=[InputRequired()])
     password = PasswordField('password', validators=[InputRequired(), Length(min=5, max=20)])
     remember = BooleanField('Remember Me')
+
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email')])
+    username = StringField('username', validators=[InputRequired()])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=5, max=20)])
