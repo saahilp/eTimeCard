c1cdc006d96e40765ab90e800f3033cf5ba34183
Shreeyan Shayan <sshayan@purdue.edu>
2017-12-08 08:39:50 -0500

    Database

--- app.py
+++ app.py
@@ -23,8 +23,8 @@ Bootstrap(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True)
-    email = db.Column(db.String(30), unique=True)
-    password = db.Column(db.String(30))
+    email = db.Column(db.String(36), unique=True)
+    password = db.Column(db.String(88))
 
 @app.route('/')
 def index():
