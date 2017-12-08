72ba2f2000291ff0d451e10940545f77097e5c8d
Shreeyan Shayan <sshayan@purdue.edu>
2017-12-08 10:08:08 -0500

    Login

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def login():
 
 
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
+        user = User.query.filter_by(username='ramyak').first()
         if user:
             #if check_password_hash(user.password, form.password.data):
                 #login_user(user, remember=form.remember.data)
