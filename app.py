a9a41c1329dfa4b6c82274fb4bb25f8127736425
Shreeyan Shayan <sshayan@purdue.edu>
2017-12-08 12:22:17 -0500

    Bug

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ login_manager.init_app(app)
 login_manager.login_view = 'login'
 
 
-class User(UserMixin, Udb.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True)
     email = db.Column(db.String(36), unique=True)
