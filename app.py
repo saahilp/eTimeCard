9fd518020b09dc765cd56ff5232e5f9d77a0217a
Shreeyan Shayan <sshayan@purdue.edu>
2017-12-08 12:59:51 -0500

    Dashboard bug fix

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(36), unique=True)
     password = db.Column(db.String(256))
 
-class Timestamps(db.Model):
+class Timestamps(UserMixin, db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20))
